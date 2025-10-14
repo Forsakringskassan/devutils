7c7d4ac6bb052d1e464950bfe90c4c1fe7e68af9
Tomas Bjerre <tomas.bjerre85@gmail.com>
2025-10-14 11:22:41 +0200

    fix

--- bin/clone_and_push_template.sh
+++ bin/clone_and_push_template.sh
@@ -43,7 +43,7 @@ git clone $newrepo $TMPFOLDER \
 && cd $TMPFOLDER \
 && git remote add tmpl $tmplrepo \
 && git fetch tmpl \
-&& (git checkout tmpl/main 2>/dev/null || git checkout tmpl/master .) \
+&& (git checkout -b main tmpl/main 2>/dev/null || git checkout -b main tmpl/master .) \
 && echo "" > CHANGELOG.md \
 && git add . \
 && tmpl_ref=$(git rev-parse tmpl/main 2>/dev/null || git rev-parse tmpl/master 2>/dev/null) \
